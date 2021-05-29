b723f4a67219402024dae2aebd6d8a659ef6a7be
Leonid Charikov <leonidcharikov@gmail.com>
2021-05-29 13:10:41 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        File file = new File("M:\p.txt");
+        try{
+            Scanner in = new Scanner(new FileInputStream(file)); 
+           int slova = 0;
+            while(in.hasNext()){
+                in.next();
+                slova++;
+            }
+        System.out.println("Poèet slov: " + slova);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Soubor nebyl nalezen");
+        }
+    }
+
+}
