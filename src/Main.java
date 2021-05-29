62d7100f4a69244bc4f585efc31f2bf0dc4c203d
Leonid Charikov <leonidcharikov@gmail.com>
2021-05-29 13:50:23 +0200

    Final Changes

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.io.Reader;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
+      public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
+
         File file = new File("C:\\m.txt");
-        try{
-            Scanner in = new Scanner(new FileInputStream(file)); 
-           int slova = 0;
-            while(in.hasNext()){
-                in.next();
-                slova++;
-            }
-        System.out.println("Poèet slov: " + slova);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            System.out.println("Soubor nebyl nalezen");
-        }
+        FileInputStream stream = new FileInputStream(file);
+        InputStreamReader in = new InputStreamReader(stream);
+        BufferedReader reader = new BufferedReader(in);
+
+
+        String txt;
+        int slovo = 0;
+        int mezera = 0;
+
+          while((txt = reader.readLine()) != null) {
+              String[] counter = txt.split("\\s+");
+              slovo = slovo + counter.length;
+              mezera += slovo -1;
+          }
+        System.out.println("Pocet mezer: " + mezera);
+          System.out.println("Pocet slov: " + slovo);
+
+
     }
 
 }
