e8d2b378e91121d5ee5b8f6a4c66410b1951485b
Leonid Charikov <leonidcharikov@gmail.com>
2021-05-29 13:34:13 +0200

    Final Changes

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        File file = new File("M:\p.txt");
+        File file = new File("C:\\m.txt");
         try{
             Scanner in = new Scanner(new FileInputStream(file)); 
            int slova = 0;
